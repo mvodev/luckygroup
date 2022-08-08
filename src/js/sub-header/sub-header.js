127cac4c249b57002eab9ef2738e0034129222c2
Vadim Manushin <vadimmanushin@yandex.ru>
2022-08-08 13:11:52 +0300

    added getting data from outer source for header

---
+++ src/js/sub-header/sub-header.js
@@ -0,0 +1,11 @@
+const getSubHeader = async ()=> {
+  let subHeader = '';
+  const result = await fetch('https://baconipsum.com/api/?type=lucky')
+    .then(response => response.json());
+  if(result && result.length > 0){
+    subHeader = result[0];
+  }
+  const subHeaderInDOM = document.querySelector('.js-sub-header');
+  subHeaderInDOM.innerText = subHeader;
+}
+getSubHeader();
