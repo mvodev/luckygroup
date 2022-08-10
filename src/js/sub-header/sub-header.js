6d7cb6d28934ee73b7ad701e4627132044abcefe
Vadim Manushin <vadimmanushin@yandex.ru>
2022-08-10 22:41:06 +0300

    changed structure of tags in content

--- src/js/sub-header/sub-header.js
+++ src/js/sub-header/sub-header.js
@@ -2,7 +2,7 @@ const getSubHeader = async ()=> {
   let subHeader = '';
   const result = await fetch('https://baconipsum.com/api/?type=lucky')
     .then(response => response.json());
-  if(result && result.length > 0){
+  if(result && result[0]){
     subHeader = result[0];
   }
   const subHeaderInDOM = document.querySelector('.js-sub-header');
