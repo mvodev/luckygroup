fda4c5970a9c33f117b4575e74b020c835679c01
Vadim Manushin <vadimmanushin@yandex.ru>
2022-08-15 14:53:05 +0300

    changed structure of scripts

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,12 @@ import '../../node_modules/bootstrap/js/src/dropdown';
 import '../styles/index.scss';
 import './burger/burger.js';
 import './sub-header/sub-header.js';
+import getSubHeader from '../js/sub-header/sub-header';
+import Burger from './burger/burger';
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  document.querySelectorAll('.js-header__burger').forEach(elem => new Burger(elem));
+	getSubHeader();
+
+});
